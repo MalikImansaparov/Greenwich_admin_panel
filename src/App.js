f80813d6c2f4d24dada1a59a73c232821e2b68e2
imansaparov <malikimansaparov@gmail.com>
2022-01-17 21:33:56 +0600

    init: add initiall code

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Button from '@mui/material/Button';
+
+function App() {
+  return (
+      <Button variant="contained" color="primary">
+        Hello World
+      </Button>
+  );
+}
+
+export default App;
