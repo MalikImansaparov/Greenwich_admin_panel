253bb76eb4d11d9efab56d7bdce1e152e17a825a
imansaparov <malikimansaparov@gmail.com>
2022-06-24 00:57:06 +0600

    refacot: delete unnesesary code

--- src/components/signinAuth/validateForm.js
+++ src/components/signinAuth/validateForm.js
@@ -1,5 +1,4 @@
 import * as Yup from 'yup';
-// const phoneRegExp = /^((\\+[1-9]{1,4}[ \\-]*)|(\\([0-9]{2,3}\\)[ \\-]*)|([0-9]{2,4})[ \\-]*)*?[0-9]{3,4}?[ \\-]*[0-9]{3,4}?$/
 
 export const validationSchema = Yup.object().shape({
   first_name: Yup.string().required('Имя обязательный'),
@@ -9,7 +8,6 @@ export const validationSchema = Yup.object().shape({
     .min(9, 'Не правилный номер')
     .max(14, 'Не правилный номер'),
   role: Yup.string().required('Роль обязательный'),
-  // salary: Yup.number().required('Зарплата обязательный'),
   password: Yup.string()
     .required('Пароль обязательный')
     .min(6, 'Небезопасный пароль'),
