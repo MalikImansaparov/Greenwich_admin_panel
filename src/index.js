4b4599eb01a53865ce4f496c2b34b5706aa3e1f8
Imansaparov <Malikimansaparov@gmail.com>
2023-04-24 22:16:27 +0600

    fix: change app.js path

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
-import App from './App';
+import App from './app';
 import CssBaseline from '@mui/material/CssBaseline';
 import ThemeProvider from '@mui/material/styles/ThemeProvider';
 import theme from './theme';
